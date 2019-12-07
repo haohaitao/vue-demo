eeecb31474870559324f984adcd746261a8495ee
haohaitao <596336501@qq.com>
2019-12-07 23:53:28 +0800

    use vue reset file

---
+++ src/element/index.js
@@ -0,0 +1,16 @@
+// 导入自己需要的组件
+import { Select, Option, OptionGroup, Input,Button, Tree, Dialog, Row, Col } from 'element-ui'
+const element = {
+  install: function (Vue) {
+    Vue.use(Select)
+    Vue.use(Option)
+    Vue.use(OptionGroup)
+    Vue.use(Input)
+    Vue.use(Tree)
+    Vue.use(Dialog)
+    Vue.use(Row)
+    Vue.use(Button)
+    Vue.use(Col)
+  }
+}
+export default element
