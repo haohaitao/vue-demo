ae8039ac4746fc54bb06d23911981fe143507923
haohaitao <596336501@qq.com>
2020-01-14 22:07:52 +0800

    修改about，集成websocket

--- src/common/config.js
+++ src/common/config.js
@@ -1,3 +1,4 @@
 export default {
-	apiUrl: "https://www.xxxx.com"  //你的接口域名
+	apiUrl: "https://www.xxxx.com",  //你的接口域名
+	WebsocketUrl: "wss://xxx.com" //websocket服务器地址
 }
--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -1,6 +1,20 @@
 <template>
 <div class="wrapper animated bounceInDown">
-    <div v-html="content" class="content">
+    <div class="content">
+        <p><img style="margin: 0 auto;" src="https://www.haoht123.com/wp-content/uploads/2019/04/about20190403.jpg" alt="关于"  width="690" height="388" srcset="https://www.haoht123.com/wp-content/uploads/2019/04/about20190403.jpg 690w, https://www.haoht123.com/wp-content/uploads/2019/04/about20190403-300x169.jpg 300w" sizes="(max-width: 690px) 100vw, 690px" /></p>
+        <h2>关于本站</h2>
+        <p>使用vue全家桶写的前端页面</p>
+        <p>主要用到的技术：vue-cli + vue2 + vuex + vue-router + axios + element-ui</p>
+        <p>代码已开源，页脚有github的入口，欢迎交流，共同学习</p>
+        <h2>关于博主</h2>
+        <p>曾梦想仗剑走天涯，看一看世界的繁华，后来工作太忙没去。</p>
+        <h2>送给自己</h2>
+        <p>你好哇，hht。</p>
+        <p>虽然生活总是不尽人意，但是活着就有希望！</p>
+        <p>困难像弹簧，你弱它就强！</p>
+        <h2>联系方式</h2>
+        <p>Email：haohaitao28#163.com  #换@</p>
+        <p>微信：hht28_</p>
     </div>
 </div>
 
@@ -13,32 +27,9 @@ let rootUrl = config.default.apiUrl;
 export default {
   data () {
     return {
-        content:''
     };
   },
-  computed: {
-  },
-  created(){
-      this.getContent()
-  },
   methods: {
-      result(){
-          this.$message.error('数据获取失败');
-      },
-      getContent(){
-          http.get('api/wp-json/wp/v2/pages/88','','').then( (res)=> {
-              if(res.status === 200){
-                  this.content = res.data.content.rendered
-              }else{
-                  this.result();
-              }
-          })
-      }
-  },
-  watch: {
-
-  },
-  components: {
   },
 };
 </script>
