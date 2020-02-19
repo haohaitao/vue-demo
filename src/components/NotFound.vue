b3ddaaba6f172f440eda5e88ee4c4d7809c512fd
haohaitao <596336501@qq.com>
2020-02-19 14:07:19 +0800

    调整404页面

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,11 +1,16 @@
 <template>
   <main>
     <section>
-      <p class="number">404</p>
-      <p>很抱歉，找不到页面~~</p>
-      <router-link to="/">
-        <el-button type="warning">返回首页</el-button>
-      </router-link>
+      <p>很抱歉，我们找不到您想要的东西。</p>
+      <p>
+        您可以试试CTRL+F5刷新页面，或者点击
+        <router-link to="/">首页</router-link>进行浏览。
+      </p>
+      <div id="beastainer">
+        <img id="beast404le" src="@/assets/imgs/beast-404_LE.png" alt="404图片" />
+        <img id="beast404re" src="@/assets/imgs/beast-404_RE.png" alt="404图片" />
+        <img class="beast 404" src="@/assets/imgs/beast-404.png" alt="404图片" />
+      </div>
     </section>
   </main>
 </template>
@@ -13,10 +18,10 @@
 <script>
 export default {
   data() {
-    return {}
+    return {};
   },
   components: {}
-}
+};
 </script>
 
 <style lang='scss' scoped>
@@ -25,19 +30,132 @@ section {
   padding: 170px 0;
   margin: 0 auto;
   text-align: center;
+  overflow: hidden;
+  width:1000px;
+  @keyframes crazyrighteye {
+    0% {
+      left: 231px;
+      top: 68px;
+    }
 
-  .number {
-    color: #252932;
-    font-size: 98px;
-    font-weight: 700;
-    line-height: 150px;
-    text-shadow: rgba(61, 61, 61, 0.3) 1px 1px, rgba(61, 61, 61, 0.2) 2px 2px,
-      rgba(61, 61, 61, 0.3) 3px 3px;
+    10% {
+      left: 212px;
+      top: 62px;
+    }
+
+    20% {
+      left: 212px;
+      top: 62px;
+    }
+
+    40% {
+      left: 239px;
+      top: 64px;
+    }
+
+    50% {
+      left: 240px;
+      top: 80px;
+    }
+
+    60% {
+      left: 215px;
+      top: 73px;
+    }
+
+    70% {
+      left: 231px;
+      top: 68px;
+    }
+
+    100% {
+      left: 231px;
+      top: 68px;
+    }
+  }
+
+  @keyframes crazylefteye {
+    0% {
+      left: 125px;
+      top: 65px;
+    }
+
+    10% {
+      left: 118px;
+      top: 56px;
+    }
+
+    20% {
+      left: 118px;
+      top: 56px;
+    }
+
+    40% {
+      left: 148px;
+      top: 62px;
+    }
+
+    50% {
+      left: 145px;
+      top: 72px;
+    }
+
+    60% {
+      left: 121px;
+      top: 70px;
+    }
+
+    70% {
+      left: 125px;
+      top: 65px;
+    }
+
+    100% {
+      left: 125px;
+      top: 65px;
+    }
+  }
+  #beastainer {
+    float: right;
+    padding: 20px 0 20px 20px;
+    position: relative;
+  }
+
+  #beastainer > .beast {
+    margin: 0;
+  }
+  #beast404le,
+  #beast404re {
+    position: absolute;
+    -webkit-animation-delay: 6s;
+    animation-delay: 6s;
+    -webkit-animation-duration: 7s;
+    animation-duration: 7s;
+    -webkit-animation-iteration-count: infinite;
+    animation-iteration-count: infinite;
+    -webkit-animation-play-state: running;
+    animation-play-state: running;
+  }
+
+  #beast404le {
+    left: 125px;
+    top: 65px;
+    -webkit-animation-name: crazylefteye;
+    animation-name: crazylefteye;
+  }
+  #beast404re {
+    left: 231px;
+    top: 68px;
+    -webkit-animation-name: crazyrighteye;
+    animation-name: crazyrighteye;
   }
   p {
-    font-size: 30px;
+    font-size:18px;
+    text-align: left;
   }
-
+  a{
+        color: #3d7e9a;
+  }
   button {
     margin-top: 32px;
     font-size: 14px;
